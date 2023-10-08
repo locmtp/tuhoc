cc1921678592a9a8f08ec04c2965a4703008b200
Loc Dai <lockc2003@gmail.com>
2023-10-08 17:33:39 +0700

    update

--- tuhocjv/src/bai18_forlongnhau.java
+++ tuhocjv/src/bai18_forlongnhau.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class bai18_forlongnhau {
+public class bai18_forlongnhau {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) {
+                System.out.print(i + "" + j + "\t");
+            }
+            System.out.println("");
+        }
+        System.out.println("\n \n");
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) {
+                if (j == 1 || i > j || i == j)
+                    System.out.print(i + "" + j + "\t");
+                else
+                    System.out.print("\t");
+            }
+            System.out.println("");
+        }
+        System.out.println("\n \n");
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) {
+                if (i == j||i>j)
+                    System.out.print(i + "" + j + "\t");
+                else
+                    System.out.print("\t");
+
+            }
+            System.out.println("");
+        }
+    }
 }
--- tuhocjv/src/bai20_xulyngoaile.java
+++ tuhocjv/src/bai20_xulyngoaile.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class bai20_xulyngoaile {
+import java.rmi.AlreadyBoundException;
+import java.util.Scanner;
+
+public class bai20_xulyngoaile {
+    public static void main(String[] args) {
+        int a=10;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Nhập b: ");
+        int b= sc.nextInt();
+        try {
+            int c=a/b;
+            System.out.println("c= "+c);
+        }
+        catch (ArithmeticException ex){
+            // chỉ bắt lỗi của số học nếu k phải xuống dưới Exception bắt tất cả các lỗi
+            System.out.println("có lỗi ở đây!!");
+            ex.printStackTrace();
+        }
+        catch (Exception ex){    // bắt tất cả các lỗi
+            System.out.println("có lỗi r!!!!");
+            ex.printStackTrace();
+        }
+        finally {
+            // bỏ qua lỗi và chạy code trong này bth
+            System.out.println("có lỗi hay k thì vẫn chạy được");
+        }
+        System.out.println("Đoạn code phía sau");
+    }
 }
